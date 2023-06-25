75e65efb0d064aa0e3684c7d97947387ec7d9ab2
Sylvain Nicolet <nicoletsylvain@gmail.com>
2023-06-25 17:24:08 +0200

    Adding ex02

---
+++ ex02/srcs/main.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: synicole <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/25 17:18:04 by synicole          #+#    #+#             */
+/*   Updated: 2023/06/25 17:18:05 by synicole         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main()
+{
+	std::string	str = "HI THIS IS BRAIN";
+	std::string	*stringPTR = &str;
+	std::string	&stringREF = str;
+
+	std::cout << "--- ADDRESS ---" << std::endl;
+	std::cout << "Address of str: " << &str << std::endl;
+	std::cout << "Address of stringPTR: " << stringPTR << std::endl;
+	std::cout << "Address of stringREF: " << &stringREF << std::endl;
+
+	std::cout << std::endl;
+
+	std::cout << "--- VALUE ---" << std::endl;
+	std::cout << "Value of str: " << str << std::endl;
+	std::cout << "Value of stringPTR: " << *stringPTR << std::endl;
+	std::cout << "Value of stringREF: " << stringREF << std::endl;
+
+	return (0);
+}
